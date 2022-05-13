0338ddf9b9351e818cf01c7d6995aafdeab05890
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2022-05-13 13:54:38 +0200

    Menú aplicación 1

--- Randomaze SDL menu code/menurandomaze.c
+++ Randomaze SDL menu code/menurandomaze.c
@@ -4,15 +4,21 @@
     int main(int argcs, char *args[])
     {
         SDL_Init(SDL_INIT_VIDEO);
-        SDL_Surface *pantalla;
+        SDL_Rect posicion;
+        posicion.x= 0;
+        posicion.y= 0;
+        posicion.w= 1280;
+        posicion.h=720;
+        SDL_Surface *pantalla, *aux;
         SDL_Window *principal;
         principal = SDL_CreateWindow( "Randomaze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
         pantalla = SDL_GetWindowSurface(principal);
-        pantalla = SDL_LoadBMP( "randomaze_bueno.bmp" );
+        aux = SDL_LoadBMP( "randomaze_bueno.bmp" );
 
-        if(pantalla == NULL){
+        if(aux == NULL){
             printf("error");
         }
+        SDL_BlitSurface(aux, NULL, pantalla, &posicion);
         SDL_UpdateWindowSurface(principal);
         SDL_Delay(2000);
 
