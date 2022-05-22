6c6db8fbc334ec73d499da441d3ffacb17e91b39
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2022-05-22 18:48:08 +0200

    Aplicación de eventos en el menu. Evento Quit

--- Randomaze SDL menu code/menurandomaze.c
+++ Randomaze SDL menu code/menurandomaze.c
@@ -1,5 +1,7 @@
     #include <SDL.h>
+    #include <stdbool.h>
     #include <stdio.h>
+
 
     int main(int argcs, char *args[])
     {
@@ -11,22 +13,33 @@
         posicion.h=720;
         SDL_Surface *pantalla, *aux;
         SDL_Window *principal;
+        bool isRunning = true;
+        SDL_Event event;
+
         principal = SDL_CreateWindow( "Randomaze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
         pantalla = SDL_GetWindowSurface(principal);
         aux = SDL_LoadBMP( "randomaze_bueno.bmp" );
 
-        if(aux == NULL){
-            printf("error");
-        }
-        SDL_BlitSurface(aux, NULL, pantalla, &posicion);
-        SDL_UpdateWindowSurface(principal);
-        SDL_Delay(2000);
 
+        if(aux == NULL)
+        printf("error al cargar SDL");
+
+        while(isRunning)
+        {
+            while(SDL_PollEvent(&event)!=0)
+            {
+                if (event.type==SDL_QUIT)
+                isRunning= false;
+            }
+                SDL_BlitSurface(aux, NULL, pantalla, &posicion);
+                SDL_UpdateWindowSurface(principal);
+
+        }
+
+        SDL_FreeSurface(aux);
+        SDL_FreeSurface(pantalla);
+        SDL_DestroyWindow(principal);
+        SDL_Quit();
         return 0;
 
     }
-
-
-
-
-
