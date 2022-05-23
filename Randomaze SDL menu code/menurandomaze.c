9cff88f18723613d444706bd353a205d4ff3af5d
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2022-05-23 19:33:26 +0200

    Añadir imágenes y eventos.
    Eventos de teclado, que permiten el acceso al juego, imagen de opciones, personajes y dificultad. Aún faltan cosas por determinar.

--- Randomaze SDL menu code/menurandomaze.c
+++ Randomaze SDL menu code/menurandomaze.c
@@ -12,9 +12,18 @@
         posicion.w= 1280;
         posicion.h=720;
         SDL_Surface *pantalla, *aux;
+        SDL_Surface *imagen1;
+        SDL_Surface *imagen2;
+        SDL_Surface *imagen3;
+        SDL_Surface *imagen4;
         SDL_Window *principal;
         bool isRunning = true;
         SDL_Event event;
+        imagen1= SDL_LoadBMP("randomaze_bueno.bmp");
+        imagen2= SDL_LoadBMP("opcionesradomaze.bmp");
+        imagen3= SDL_LoadBMP("dificultadrandomaze.bmp");
+        imagen4= SDL_LoadBMP("");
+
 
         principal = SDL_CreateWindow( "Randomaze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
         pantalla = SDL_GetWindowSurface(principal);
@@ -30,12 +39,35 @@
             {
                 if (event.type==SDL_QUIT)
                 isRunning= false;
+                else if(event.type == SDL_KEYDOWN)
+                {
+                    switch (event.key.keysym.sym)
+                    {
+                    case SDLK_1:
+                        //principal= Jugar; Aún por implementar.
+                        break;
+                    case SDLK_2:
+                        aux = imagen2; //Opciones
+                        if(SDLK_3)
+                            aux = imagen3; //dificultad;
+                        else if(SDLK_4)
+                            aux = imagen4; //personajes;
+                        else if(SDLK_SPACE)
+                            aux = imagen1; //Pulsando espacio te redirige a la pantalla principal;
+                        break;
+                    }
+                }
             }
                 SDL_BlitSurface(aux, NULL, pantalla, &posicion);
                 SDL_UpdateWindowSurface(principal);
 
         }
 
+
+        SDL_FreeSurface(imagen1);
+        SDL_FreeSurface(imagen2);
+        SDL_FreeSurface(imagen3);
+        SDL_FreeSurface(imagen4);
         SDL_FreeSurface(aux);
         SDL_FreeSurface(pantalla);
         SDL_DestroyWindow(principal);
@@ -43,3 +75,7 @@
         return 0;
 
     }
+
+
+
+
