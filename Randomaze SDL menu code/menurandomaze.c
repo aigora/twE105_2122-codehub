b38134db69d3b9354339921f5471d8cf4491f474
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2022-05-25 13:55:08 +0200

    Uso de acciones de ratón. Versión 1

--- Randomaze SDL menu code/menurandomaze.c
+++ Randomaze SDL menu code/menurandomaze.c
@@ -11,6 +11,8 @@
         posicion.y= 0;
         posicion.w= 1280;
         posicion.h=720;
+        int x,y;
+        int accion;
         SDL_Surface *pantalla, *aux;
         SDL_Surface *imagen1;
         SDL_Surface *imagen2;
@@ -63,21 +65,64 @@
                         break;
                     }
                 }
+<<<<<<< Updated upstream:Randomaze SDL menu code/menurandomaze.c
             }
                 SDL_BlitSurface(aux, NULL, pantalla, &posicion);
                 SDL_UpdateWindowSurface(principal);
 
         }
+=======
 
+                if (SDL_MOUSEMOTION == event.type)
+                {
 
-        SDL_FreeSurface(imagen1);
-        SDL_FreeSurface(imagen2);
-        SDL_FreeSurface(imagen3);
-        SDL_FreeSurface(imagen4);
-        SDL_FreeSurface(aux);
-        SDL_FreeSurface(pantalla);
-        SDL_DestroyWindow(principal);
-        SDL_Quit();
-        return 0;
+                    accion=SDL_GetMouseState(&x,&y);
+                    printf(" %d %d", x,y);
 
+                }
+                else if (event.type == SDL_MOUSEBUTTONDOWN)
+                {
+>>>>>>> Stashed changes:ingame.v1/main.c
+
+                    if ( x<573 && y> 491 && x>298 && y<588) //Jugar
+                    {
+                         jugar(50);
+                    }
+                    else if( x< 957 && y>490 && x>688 && y<589) //Opciones
+                    {
+                        aux = imagen2;
+                    }
+                    else if ( x< 1144 && y> 444 && x> 771 && y<566) //Dificultad
+                    {
+                        aux = imagen3;
+                        //Facil. coordenadas,x<828,y>177;x>437,y<269
+                        //Normal. Coordenadas, x<818, y>311; x<437, y<403
+                        //Dificil. coord x<820, y>456; x>434, y<540;
+                    }
+                    else if (x<489 && y>442 && x>128 && y<567)
+                    {
+
+                    }
+                }
+            }
+            SDL_BlitSurface(aux, NULL, pantalla, &posicion);
+            SDL_UpdateWindowSurface(principal);
+        }
+
+            SDL_FreeSurface(imagen1);
+            SDL_FreeSurface(imagen2);
+            SDL_FreeSurface(imagen3);
+            SDL_FreeSurface(imagen4);
+            SDL_FreeSurface(aux);
+            SDL_FreeSurface(pantalla);
+            SDL_DestroyWindow(principal);
+            SDL_Quit();
+            return 0;
     }
+<<<<<<< Updated upstream:Randomaze SDL menu code/menurandomaze.c
+=======
+
+//int main(int argc, char* args[] ){
+//    jugar(50);
+//}
+>>>>>>> Stashed changes:ingame.v1/main.c
