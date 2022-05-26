7088f255ed695b3bce732c04d9b4b214d4bc57cd
alvarocamposupm <alvaro.campos.coria@alumnos.upm.es>
2022-05-26 17:18:22 +0200

    Cambio de antiguo menú, por nuevo menú con SDL

--- Randomazemenu.c
+++ Randomazemenu.c
@@ -1,183 +1,105 @@
-    #include<stdio.h>
-    #include<stdlib.h>
+    #include <SDL.h>
+    #include <stdbool.h>
+    #include <stdio.h>
 
 
-    void titulo();
-    void bienvenida();
-    void modosdejuego();
-    void estandar();
-    void contrareloj();
-    void dificultad();
-    void opciones();
-    void skins();
-    void salir();
-
-
-    int main()
+    int main(int argcs, char *args[])
     {
-       //Menu provisional.
-       //Falta poner el titulo del programa con un generador de letras. O implementando la libreria grafica SDL
-       titulo();
-       system("cls");
-       bienvenida();
-       system("cls");
+        SDL_Init(SDL_INIT_VIDEO);
+        SDL_Rect posicion;
+        posicion.x= 0;
+        posicion.y= 0;
+        posicion.w= 1280;
+        posicion.h=720;
+        int x, y;
+        int accion;
+        SDL_Surface *pantalla, *aux;
+        SDL_Surface *imagen1;
+        SDL_Surface *imagen2;
+        SDL_Surface *imagen3;
+        SDL_Surface *imagen4;
+        SDL_Surface *fondo;
+        SDL_Window *principal;
+        bool isRunning = true;
+        SDL_Event event;
+        imagen1= SDL_LoadBMP("randomaze_bueno.bmp");
+        imagen2= SDL_LoadBMP("opcionesradomaze.bmp");
+        imagen3= SDL_LoadBMP("dificultadrandomaze.bmp");
+        imagen4= SDL_LoadBMP("");
+
+        SDL_SetColorKey( imagen1, SDL_TRUE, SDL_MapRGB(imagen1->format, 0,255,0));
+        SDL_SetColorKey( imagen2, SDL_TRUE, SDL_MapRGB(imagen2->format, 0,255,0));
+        SDL_SetColorKey( imagen3, SDL_TRUE, SDL_MapRGB(imagen3->format, 0,255,0));
+        //SDL_SetColorKey( imagen4, SDL_TRUE, SDL_MapRGB(imagen4->format, 0,255,0));
 
 
+        principal = SDL_CreateWindow( "Randomaze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
+        pantalla = SDL_GetWindowSurface(principal);
+        aux = SDL_LoadBMP( "randomaze_bueno.bmp" );
 
 
-        return 0;
+        if(aux == NULL)
+        printf("error al cargar SDL");
 
-    }
-
-    void titulo()
-    {
-        //Faltaría la creación del titulo. Con generador de matrices, este es provisional.
-        printf("RANDOMAZE\n");
-    }
-
-    void bienvenida()
-    {
-        char selecciona;
-        printf("Bienvenido a RandoMaze\n");
-        printf("Seleccione alguna de las opciones:\n");
-        printf("1-Jugar(J)\n");
-        printf("2-Opciones(O)\n");
-
-
-        do
+        while(isRunning)
         {
-        scanf(" %c" , &selecciona);
+            while(SDL_PollEvent(&event)!=0)
+            {
+                if (SDL_MOUSEMOTION == event.type)
+                    {
+                        accion=SDL_GetMouseState(&x,&y);
+                        printf(" %d %d", x,y);
 
-            switch(selecciona)
+                    }
+                else if (event.type == SDL_QUIT)
                 {
-                case 'J':
-                case 'j':
-                modosdejuego();
-                break;
-                case 'O':
-                case 'o':
-                opciones();
-                break;
+                    isRunning = false;
+                }
+                else if (event.type == SDL_MOUSEBUTTONDOWN)
+                {
+                    if ( x<573 && y> 491 && x>298 && y<588) //Jugar
+                        {
+                            //jugar(50);
+                        }
+                    else if( x< 957 && y>490 && x>688 && y<589) //Opciones
+                        {
+                            aux = imagen2;
+                        }
+                    else if ( x< 1144 && y> 444 && x> 771 && y<566) //Dificultad
+                        {
+                            aux = imagen3;
+                            //Facil. coordenadas,x<828,y>177;x>437,y<269
+                            //Normal. Coordenadas, x<818, y>311; x<437, y<403
+                            //Dificil. coord x<820, y>456; x>434, y<540;
+                        }
+                    else if (x<489 && y>442 && x>128 && y<567)
+                        {
+                            //if ()
 
-                default:
-                printf("seleccione una opcion valida\n");
-                break;
+                        }
+                        else if(x<1277 && x>1088 && y<73 && y>3)
+                        {
+                            aux= imagen1;
+                        }
+                }
+            }
+
+            SDL_BlitSurface(aux, NULL, pantalla, &posicion);
+            SDL_UpdateWindowSurface(principal);
         }
-        }while (selecciona!='J'||selecciona!='j'||selecciona!='O'||selecciona!='o'||selecciona!='V'||selecciona!='v');
 
+            SDL_FreeSurface(imagen1);
+            SDL_FreeSurface(imagen2);
+            SDL_FreeSurface(imagen3);
+            SDL_FreeSurface(imagen4);
+            SDL_FreeSurface(aux);
+            SDL_FreeSurface(pantalla);
+            SDL_DestroyWindow(principal);
+            SDL_Quit();
+            return 0;
     }
-    void modosdejuego()
-    {
-        char selecciona;
-        printf("Selecciona entre los modos de jeugo disponibles:\n");
-        printf("1-Contrareloj(C)\n");
-        //Aun estamos trabajando en ello
-        printf("2-estandar(E)\n");
-        //Aun estamos trabajanod en ella
-        printf("3-Volver(V)\n");
-        scanf(" %c", &selecciona);
-        do
-        {
-        switch(selecciona)
-        {
-        case 'C':
-        case 'c':
-            contrareloj();
-            break;
-        case 'E':
-        case 'e':
-            estandar();
-            break;
-        case 'V':
-        case 'v':
-            main();
-        default:
-            printf("seleccione una opcion valida\n");
-        }
-        }while (selecciona!='C'||selecciona!='c'||selecciona!='E'||selecciona!='e'||selecciona!='V'||selecciona!='v');
-    }
-    void estandar()
-    {
-        dificultad();
-        //Aquí es cuando se iniciaría el programa en si, la generación de matrices. Con un modo estandar
-        //aun trabajando en ella.
-    }
-    void contrareloj()
-    {
-        dificultad();
-        //Aquí es cuando se iniciaría el programa en si, la generación de matrices. Con un modo contrareloj
-        //Tiempo y alguna opcion más aún en discusión.
-        //aun trabajando en ella.
-    }
-    void dificultad()
-    {
-        char selecciona;
-        printf("Selecciona una dificultad\n");
-        printf("Fácil(1)\n");
-        printf("Normal(2)\n");
-        printf("Difícil(3)\n");
-        do
-        {
-            scanf(" %c", &selecciona);
-        switch(selecciona)
-        {
-        case '1':
-            //
-            break;
-        case '2':
-            //
-            break;
-        case '3':
-            //
-            break;
-        case 'V':
-        case'v':
-            modosdejuego();
-        default:
-            printf("seleccione una opcion valida\n");
-        }
-        } while (selecciona!='1'||selecciona!='2'||selecciona!='3'||selecciona!='V'||selecciona!='v');
-
-    }
-    void opciones()
-    {
-        char selecciona;
-        printf("Seleccione alguna de estas opciones:\n");
-        printf("Skins(1)\n");
-        printf("Salir(2)\n");
-
-        do
-        {
-        scanf(" %c", selecciona);
-        switch(selecciona)
-        {
-        case '1':
-            //
-            break;
-        case '2':
-            //
-            break;
-        case 'V':
-        case 'v':
-            bienvenida();
-            break;
-        default:
-            printf("seleccione una opcion valida\n");
-        }
-        } while (selecciona!='1'||selecciona!='2'||selecciona!='V'||selecciona!='v');
 
 
-
-    }
-
-    void skins()
-    {
-        //Aun trabajando en ello con libreria SDL
-    }
-    void salir()
-    {
-        main();
-    }
 
 
 
